09ca32b39c26bd1ae48cc1ba59028dc6bbe81304
AriBhizer <arisiraj738@gmail.com>
2022-07-08 21:07:04 +0800

    hamzz

---
+++ plugins/donasi.js
@@ -0,0 +1,8 @@
+let handler =  m => m.reply(`
+sementara tidak open donasi
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
